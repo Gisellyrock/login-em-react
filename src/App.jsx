c91e1983be1978df17ac67d8b8906a2c5cb9c749
GisellyRock <gisellydsr@gmail.com>
2023-06-27 17:33:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import GlobalStyle from './styles/global';
+import RoutesApp from './routes';
+import { AuthProvider } from './contexts/auth';
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <RoutesApp />
+        <GlobalStyle />
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
